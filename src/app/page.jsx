d0e9e78f631f9b8e13be85918d6e2986f12af650
Lshiroc <zeynalmerdanli4321@gmail.com>
2024-01-18 20:34:41 +0400

    added command list, checking system

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,9 +5,17 @@ import { useState } from 'react';
 export default function Home() {
 
   const [usedCommands, setUsedCommands] = useState([]);
+  const [commandList, setCommandList] = useState(["sudo su", "mkdir", "ls -la", "nmap -p- 10.20.3.4", "clear", "rm -f file.txt", "rf myFolder"]);
+  const [queue, setQueue] = useState(0);
+  const [correct, setCorrect] = useState(true);
 
   const addCommand = (e) => {
+    // Check if user Entered
     if(e.keyCode == 13) {
+      if(e.target.value == commandList[queue]) {
+        setQueue(queue + 1);
+      }
+
       if(e.target.value == "clear") {
         setUsedCommands([]);
         e.target.value = "";
@@ -15,6 +23,13 @@ export default function Home() {
         const command = e.target.value;
         setUsedCommands([...usedCommands, command]);
         e.target.value = "";
+      }
+    } else {
+      // Check if word is correct
+      if(commandList[queue].startsWith(e.target.value)) {
+        setCorrect(true);
+      } else {
+        setCorrect(false);
       }
     }
   }
@@ -31,8 +46,15 @@ export default function Home() {
         </div>
         <div className={style.inp}>
           <div className={style.label}><span className={style.userDefine}>root@mycomputer</span>:~$</div>
-          <input className={style.terminalInput} autoCorrect={false} spellCheck={false} onKeyDown={(e) => addCommand(e)} type="text" />
+          <input className={style.terminalInput} spellCheck={false} onKeyUp={(e) => addCommand(e)} type="text" />
         </div>
+      </div>
+      <div className={style.words}>
+          {
+            commandList.map((command, index) => (
+              <div key={index} className={`${style.word} ${index == queue && correct ? style.true : index == queue && !correct && style.wrong}`}>{command}</div>
+            ))
+          }
       </div>
     </main>
   )
